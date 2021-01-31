68a5ea8eb507c4aff0c0358222f7737f03b499a7
Colin Tinney <cdtinney@users.noreply.github.com>
2021-01-31 11:00:54 -0800

    initial commit

---
+++ src/main/java/org/ctinney/spune/SpuneApplication.java
@@ -0,0 +1,13 @@
+package org.ctinney.spune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ctinney/spune/SpuneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ctinney.spune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
